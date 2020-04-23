c84fc5b614ae449fa2a0aef18a8e413ee25ec26f
Dion Coder <jellopops@gmail.com>
2020-04-23 15:33:09 +0700

    updating linkedlists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -20,10 +20,11 @@ class LinkedList {
 
     size() {
         let count = 0;
+        let node = this.head;
 
-        while (this.head) {
+        while (node) {
             count++;
-            this.head = this.head.next;
+            node = node.next;
         }
 
         return count;
@@ -59,7 +60,144 @@ class LinkedList {
 
         this.head = this.head.next;
     }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        if (this.size() == 1) {
+            this.head = null;
+
+            return;
+        }
+    
+        if (this.size() > 1) {
+            let previous = this.head;
+            let node = this.head.next;
+
+            while (previous) {
+                if (node && !node.next) {
+                    return previous.next = null;
+                }
+                
+                previous = this.head.next
+                node = node.next;
+            }
+        }
+    }
+
+    insertLast(data) {
+        const lastNode = this.getLast();
+
+        if (!lastNode) {
+            this.head = new Node(data);
+        } else {
+            lastNode.next = new Node(data);
+        }
+    }
+
+    getAt(idx) { 
+        if (idx > this.size()) {
+            return null;
+        }
+
+        let node = this.head;
+        let counter = 0;
+
+        while (node) {
+            if (counter == idx) {
+                return node;
+            }
+            
+            counter++;
+            node = node.next;
+        }
+
+        return null;
+    }
+
+    removeAt(idx) {
+        if (!this.head) {
+            return;
+        }
+
+        if (idx == 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        if (this.size() == 1) {
+            this.head = null;
+        }
+
+        if (idx == this.size() - 1) {
+            this.removeLast();
+        }
+
+        var previous = this.getAt(idx - 1);
+
+        if (!previous || !previous.next) {
+            return;
+        }
+
+        previous.next = previous.next.next;
+    }
+
+    insertAt(data, idx) {
+        if (!this.head) {
+            this.head = new Node(data);
+
+            return;
+        }
+
+        if (idx == 0) {
+            var previous = this.head;
+
+            if (previous) {
+                this.head = new Node(data, previous);
+            }
+
+            return;
+        }
+
+        if (idx == this.size() || idx > this.size()) {
+            var previous = this.getLast();
+
+            previous.next = new Node(data);
+
+            return;
+        }
+
+        var previous = this.getAt(idx - 1);
+
+        if (previous) {
+            var newNode = new Node(data);
+
+            newNode.next = previous.next ? previous.next : null;
+            previous.next = newNode;
+        }
+    }
+
+    forEach(func) {
+        let node = this.head;
+        let counter = 0;
+
+        while (node) {
+            func(node, counter);
+            node = node.next;
+            counter++;
+        }
+    }
+
+    *[Symbol.iterator]() {
+        let node = this.head;
+
+        while(node) {
+            yield node;
+            node = node.next;
+        }
+    }
 }
 
 module.exports = { Node, LinkedList };
- 
